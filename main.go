f5046004feefd3699e44ce4fca9e470ed4bc5781
k07g <korenaga.makoto@ea-sys.jp>
2026-03-28 12:36:37 +0900

    グレースフルシャットダウンを追加

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/k07g/g2/handler"
 	"github.com/k07g/g2/infrastructure/inmemory"
@@ -23,8 +28,30 @@ func main() {
 	h := handler.NewTaskHandler(uc)
 	h.Register(mux)
 
-	log.Printf("server starting on :%s", port)
-	if err := http.ListenAndServe(":"+port, mux); err != nil {
-		log.Fatalf("server error: %v", err)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
+
+	go func() {
+		log.Printf("server starting on :%s", port)
+		if err := srv.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("shutdown error: %v", err)
+	}
+
+	log.Println("server stopped")
 }
